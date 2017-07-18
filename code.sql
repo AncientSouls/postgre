53205eea509275f51a2be3b483cd7e2ff689658e
Menzorg <menzorg@gmail.com>
2017-07-19 01:09:24 +0400

    fix(schema): schema changed, comments added

--- code.sql
+++ code.sql
@@ -11,21 +11,6 @@ create table paths (
     type text
 );
 
-create table paths2 (
-    id SERIAL,
-    a integer, /*db/table/id*/
-    b text, /*db/table/id*/
-    type text
-);
-create table spreadable (
-    source text, /*spreads: db/table*/
-    target text, /*paths: db/table*/
-    pathFromKey text, /*keyName*/
-    pathFromPrefix text default '', /*db/table*/
-    pathToKey text, /*keyName*/
-    pathToPrefix text default '', /*db/table*/
-    pathIdKey text /*keyName*/
-);
 create table spreads (
     id serial,
     target text, /*db/table/id*/
@@ -35,23 +20,54 @@ create table spreads (
     "spreaded" boolean DEFAULT false
 );
 
-CREATE FUNCTION spreadBySpread(spreadRef text) RETURNS void
+create table graph (
+    id serial
+);
+
+create table spreadable (
+    id serial,
+    pathsGraphRef text, /*spreads: db/table*/
+    spreadsGraphRef text /*paths: db/table*/
+);
+
+create table graphFields (
+    graphRef text,
+    tableRef text,
+    idField integer,
+    fromField text, /*ref|id*/
+    toField text, /*ref|id*/
+    fromTable text, /*db|db/table*/
+    toTable text, /*db|db/table*/
+);
+
+insert into paths (source, target) values ('menzorg/documents/4','menzorg/documents/3'), ('menzorg/documents/3', 'menzorg/documents/2');
+insert into graphs (id) values 1,2;
+insert into spreadable (pathsGraphRef, spreadsGraphRef) values 
+('menzorg/graphs/1', 'menzorg/graphs/2');
+insert into graphFields (graphRef, tableRef, idField, fromField, toField, fromTable, toTable) values 
+('menzorg/graphs/1', 'menorg/paths' 'id', 'source', 'target'),
+('menzorg/graphs/2', 'menorg/spreads' 'id', '', 'target');
+
+/* Распределиться на один шаг от данного спреда, по всем возможным спредеблам
+CREATE FUNCTION spreadBySpread(spreadRef text) RETURNS void */
+
+/* Распределиться на один шаг от данного спреда, согласно указанному спредеблу */
+CREATE FUNCTION _spreadBySpread(spreadRef text, spreadableRef text) RETURNS void
     AS $$ 
     	DECLARE 
         	spread record;
-            spreadables record;
+            spreadable record;
     	BEGIN
         	WHILE true LOOP
             
                 SELECT * into spread from spreads where 
                 	spreaded is not true and (
-                    id = cast (split_part(spreadRef,'/', 3) as integer) or 
-                    root = cast (split_part(spreadRef,'/', 3) as integer)
+                    id = cast (split_part(spreadRef,'/', 3) as integer)
                 )  LIMIT 1;
                 
                 if spread is null then EXIT; end if; 
 
-                FOR spreadables in (select * from spreadable where source = split_part(spreadRef,'/', 1)||'/'||split_part(spreadRef,'/', 2)) LOOP
+                select * from spreadable where id = split_part(spreadableRef,'/', 3) in spreadable;
                     EXECUTE E'
                         insert into spreads (target, root, path, prev)
                             select \''||spreadables.pathToPrefix||E'\'||cast('||spreadables.pathToKey||E' as text) as target,
@@ -63,26 +79,9 @@ CREATE FUNCTION spreadBySpread(spreadRef text) RETURNS void
                             and cast('||spreadables.pathToKey||' as text) not in (
                                 select target from '|| split_part(spreadables.source, '/', 2)||'
                             );';
-        		END LOOP;
         		Update spreads SET spreaded = true WHERE id = spread.id;
     		END LOOP; 
     	end;
     $$ LANGUAGE plpgsql;
-
-insert into paths (source, target) values ('menzorg/documents/4','menzorg/documents/3'), ('menzorg/documents/2', 'menzorg/documents/1');
-insert into paths2 (a, b) values (2,'menzorg/documents/3');
-insert into spreadable (source, target, pathFromKey, pathToPrefix, pathToKey) values 
-('menzorg/spreads', 'menzorg/paths', 'source', '', 'target'), 
-('menzorg/spreads', 'menzorg/paths2', 'b', 'menzorg/documents/', 'a');
-    
-DO $$
-DECLARE 
-root text;
-BEGIN
-
-	insert into spreads (target) values ('menzorg/documents/4') RETURNING 'menzorg/spreads/'||id INTO root;
-    PERFORM * FROM spreadBySpread(root);
-    
-END$$;
 
 select * from spreads;
